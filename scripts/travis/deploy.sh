e4dbdce046ecbc9a2ace0c1b4988dc41d357376e
Mikhail Rodichenko <Mikhail_Rodichenko@epam.com>
2019-06-24 19:25:28 +0300

    Travis CI: publishing latest version; docker login fixed

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -5,12 +5,14 @@ BUILD_VERSION=${WW_VERSION%.*}.$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT
 
 if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   echo "Deploying docker image: $BUILD_VERSION"
-  docker login -u dockerhub_login -p dockerhub_pass
+  docker login -u "$dockerhub_login" -p "$dockerhub_pass"
   docker tag wdl-workspace:build lifescience/wdl-workspace:$BUILD_VERSION
+  docker tag wdl-workspace:build lifescience/wdl-workspace:latest
   docker push lifescience/wdl-workspace:$BUILD_VERSION
+  docker push lifescience/wdl-workspace:latest
 elif [[ "$TRAVIS_BRANCH" == "develop" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
   echo "Deploying docker image: develop"
-  docker login -u dockerhub_login -p dockerhub_pass
+  docker login -u "$dockerhub_login" -p "$dockerhub_pass"
   docker tag wdl-workspace:build lifescience/wdl-workspace:develop
   docker push lifescience/wdl-workspace:develop
 else
